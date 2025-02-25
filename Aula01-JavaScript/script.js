9fcea1faf656bbc5438484ddf9582aae91300bbe
LeonardoBrito-XLBR <138629727+LeonardoBrito-XLBR@users.noreply.github.com>
2025-02-25 11:50:08 -0300

    Add files via upload

---
+++ Aula01-JavaScript/script.js
@@ -0,0 +1,9 @@
+const buttonCoverter = document.querySelector("button")
+let inputBrl = document.getElementById("brl")
+let inputUsd = document.getElementById("usd")
+
+
+
+inputUsd;addEventListener("keyup", () =>{
+    console.log(inputUsd.value)
+})
